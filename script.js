b4952478688d2d0a052586771b7fd7cac91b9523
Z A <zainibasalman@gmail.com>
2025-11-05 19:21:24 -0700

    fixed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menuIcon = document.getElementById("menu-icon");
 const sidebar = document.getElementById("sidebar");
 
@@ -12,18 +11,3 @@ links.forEach(link => {
     sidebar.classList.remove("active");
   });
 });
-=======
-const menuIcon = document.getElementById("menu-icon");
-const sidebar = document.getElementById("sidebar");
-
-menuIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
-
-const links = sidebar.querySelectorAll("a");
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    sidebar.classList.remove("active");
-  });
-});
->>>>>>> d0e0d4b (firsts)
